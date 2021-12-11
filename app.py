3c686243e698f770852f6fc3d07f5864cb5b283f
ANMi <94172090+ramadhanaraz@users.noreply.github.com>
2021-12-12 00:35:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import PIL
 
 app = Flask(__name__)
 
