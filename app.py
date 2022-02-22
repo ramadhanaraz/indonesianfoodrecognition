40a92399410287364e86b3277092c3003a313fbf
ANMi <hotseigan@gmail.com>
2022-02-22 09:41:20 +0700

    Reformated using AutoPep8

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ import PIL
 app = Flask(__name__)
 
 class_names = {0: 'Ayam Bakar',
-  1: 'Ayam Geprek',
- 2: 'Mie Goreng',
- 3: 'Nasi Goreng',
- 4: 'Sate'}
+               1: 'Ayam Geprek',
+               2: 'Mie Goreng',
+               3: 'Nasi Goreng',
+               4: 'Sate'}
 
 model = load_model('Gambar_retrain.h5')
 
 model.make_predict_function()
 
+
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(300,300))
+	i = image.load_img(img_path, target_size=(300, 300))
 	i = image.img_to_array(i)/255.0
 	i = np.expand_dims(i, 0)
 	p = model.predict(i)
@@ -30,18 +31,19 @@ def main():
 	return render_template("index.html")
 
 
-@app.route("/submit", methods = ['GET', 'POST'])
+@app.route("/submit", methods=['GET', 'POST'])
 def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
 
-		img_path = "static/" + img.filename	
+		img_path = "static/" + img.filename
 		img.save(img_path)
 
 		p = predict_label(img_path)
 
-	return render_template("index.html", prediction = p, img_path = img_path)
+	return render_template("index.html", prediction=p, img_path=img_path)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
 	#app.debug = True
-	app.run(debug = False)
+	app.run(debug=False)
